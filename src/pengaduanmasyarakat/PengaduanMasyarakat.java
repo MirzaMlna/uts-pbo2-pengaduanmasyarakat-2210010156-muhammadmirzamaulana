eca663d9334b7dc7a87bedc2b596ae8c5a971e54
MirzaMlna <mirzamaulana713@gmail.com>
2024-11-14 20:24:46 +0800

    💄 Add Main Frame

--- src/pengaduanmasyarakat/PengaduanMasyarakat.java
+++ src/pengaduanmasyarakat/PengaduanMasyarakat.java
@@ -5,6 +5,7 @@
 package pengaduanmasyarakat;
 
 import config.ConnectDB;
+import forms.MainFrame;
 
 /**
  *
@@ -16,27 +17,8 @@ public class PengaduanMasyarakat {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-//    ConnectDB db = new ConnectDB();
-//    boolean isDuplicate = db.DuplicateKey("tableName", "primaryKey", "dataValue");
-//    System.out.println("Is Duplicate: " + isDuplicate);
         
-//    ConnectDB crud = new ConnectDB();
-//    System.out.println(crud.duplicateKey("pelapor", "id_pelapor", "P002"));
-//    System.out.print(crud);
-
-//    ConnectDB crud = new ConnectDB();
-//    String[] fields = {"judul_laporan", "isi_laporan"};
-//    String[] values = {"Kemalingan", "Kemalingan Motor"};
-//    crud.addDataDynamically("laporan", "id_pelapor", "P002", fields, values);
-//    }
-
-//    ConnectDB crud = new ConnectDB();
-//    String[] fields = {"nama"};
-//    String[] values = {"Dewi"};
-//    crud.updateDataDynamically("pelapor", "id_pelapor", "P001", fields, values);
-    
-//    ConnectDB crud = new ConnectDB();
-//    crud.deleteDataDynamically("pelapor", "id_pelapor", "P002");
-//    
+        new MainFrame().setVisible(true);
+        
     }
 }
